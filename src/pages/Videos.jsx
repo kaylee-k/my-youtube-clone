967229165e89abb94b7002a86e2c55131042bc63
kaylee-k <kaylee.kihm@gmail.com>
2024-07-27 21:47:47 -0600

    Add Youtube API context for centralized API management and easier dependency injection

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,21 +2,18 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard';
-import Youtube from '../api/youtube';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
 
 export default function Videos() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos,
   } = useQuery({
     queryKey: ['videos', keyword],
-
-    queryFn: () => {
-      const youtube = new Youtube();
-      return youtube.search(keyword);
-    },
+    queryFn: () => youtube.search(keyword),
   });
 
   return (
