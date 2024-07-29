b6f3ebe8ed5ddaa8b252bd8a7fb78b2f87ef50ae
kaylee-k <kaylee.kihm@gmail.com>
2024-07-29 03:28:05 -0600

    style: Enhance VideoCard and Videos components with Tailwind CSS

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -18,11 +18,14 @@ export default function Videos() {
 
   return (
     <>
-      <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>;
+      <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong 😵‍💫</p>}
       {videos && (
-        <ul>
+        <ul
+          className='grid grid-cols-1 sm:grid-cols-2 
+        lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4'
+        >
           {videos.map((video) => (
             <VideoCard key={video.id} video={video} />
           ))}
